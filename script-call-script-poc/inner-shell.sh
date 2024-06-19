8918667a8cb2cb544ae39b1ebc120bff62801248
Gabriel Passos <gabrielsantos45725@gmail.com>
2024-06-19 19:48:20 -0300

    refact: refactor shell poc

--- script-call-script-poc/inner-shell.sh
+++ script-call-script-poc/inner-shell.sh
@@ -9,7 +9,7 @@ if [ $# -gt 0 ]; then
   VERIFY_ENV=$1
 fi
 
-echo "Searching on $FILE, if is with env: $VERIFY_ENV"
+echo "Searching on $FILE, if is using with env: $VERIFY_ENV"
 
 while IFS='=' read -r key value
 do
